9e6b698ea293320e94c01852883f1f532bff3f0a
abisi <axel.bisi@epfl.ch>
2023-07-25 16:32:28 +0100

    helper change + cca cv notebook

--- code/helpers.py
+++ code/helpers.py
@@ -68,10 +68,29 @@ def get_area_color(area):
     }
     return area_colors[area]
 
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
 
 def return_timebin_cmap(color, n_bins):
     """
     Returns n-bin colormap.
     """
-    return plt.get_cmap(color)(np.linspace(0.15, 0.85, n_bins))
+    colors = [lighten_color(color, amount=n) for n in np.linspace(0.2,1.0,n_bins)]
+    return colors
 
